910b733193adfca95d0987fb8858852c3b3233f5
nolan <paragoncdn@web507.webfaction.com>
2018-12-28 14:41:14 +0000

    initial commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/usr/local/bin/python2.7
+# EASY-INSTALL-SCRIPT: 'Django==1.8.2','django-admin.py'
+__requires__ = 'Django==1.8.2'
+import pkg_resources
+pkg_resources.run_script('Django==1.8.2', 'django-admin.py')
